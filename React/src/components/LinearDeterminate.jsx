4cde4611f66271d7834c39da925ecf1d46ea01c9
Danielson <danielson7632@gmail.com>
2024-05-20 18:05:34 +0700

    Fitur Crowdfunding

--- React/src/components/LinearDeterminate.jsx
+++ React/src/components/LinearDeterminate.jsx
@@ -2,17 +2,18 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
 
-export default function LinearDeterminate() {
+export default function LinearDeterminate(props) {
   const [progress, setProgress] = React.useState(0);
 
   React.useEffect(() => {
     const timer = setInterval(() => {
       setProgress((oldProgress) => {
-        if (oldProgress === 100) {
-          return 0;
-        }
-        const diff = Math.random() * 10;
-        return Math.min(oldProgress + diff, 100);
+        // if (oldProgress === 100) {
+        //   return 0;
+        // }
+        // const diff = Math.random() * 10;
+        // return Math.min(oldProgress + diff, 100);
+        return oldProgress
       });
     }, 500);
 
@@ -22,8 +23,8 @@ export default function LinearDeterminate() {
   }, []);
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <LinearProgress variant="determinate" value={progress} />
+    <Box sx={{ width: '80%' }}>
+      <LinearProgress color='success' variant="determinate" value={props.progress} />
     </Box>
   );
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(30)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(UserSeeder::class);
+        $this->call(CrowdfundingSeeder::class);
     }
 }
